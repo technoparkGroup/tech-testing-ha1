824d92d151ec86829758a46b6e09e0c016e5c392
Alexander Bayandin <a.bayandin@gmail.com>
2014-09-24 10:05:25 +0400

    Fix for Travis CI. Turn off notifications

--- provision/run.sh
+++ provision/run.sh
@@ -11,6 +11,8 @@ sudo apt-get install -y tarantool-lts
 
 if [ ${TRAVIS} == true ]; then
     pip install -Ur requirements.txt
+    sudo cp provision/tarantool.cfg /etc/tarantool/instances.enabled/tarantool.cfg
+    sudo cp provision/init.lua /usr/share/tarantool/lua/init.lua
 else
     sudo apt-get install -y \
         build-essential \
@@ -19,9 +21,8 @@ else
         python-pip \
         tarantool-lts
     sudo pip install -Ur ${HOME}/tech-testing-ha1/requirements.txt
+    sudo cp ${HOME}/tech-testing-ha1/provision/tarantool.cfg /etc/tarantool/instances.enabled/tarantool.cfg
+    sudo cp ${HOME}/tech-testing-ha1/provision/init.lua /usr/share/tarantool/lua/init.lua
 fi
-
-sudo cp ${HOME}/tech-testing-ha1/provision/tarantool.cfg /etc/tarantool/instances.enabled/tarantool.cfg
-sudo cp ${HOME}/tech-testing-ha1/provision/init.lua /usr/share/tarantool/lua/init.lua
 
 sudo service tarantool-lts restart
